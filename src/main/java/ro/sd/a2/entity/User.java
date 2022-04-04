8ba2c2f89e58f69092f793034851c8ec696de8e3
Rares Mihalache <raresmihalache@yahoo.com>
2022-04-04 18:17:49 +0300

    changed config: mysql -> postgresql

--- src/main/java/ro/sd/a2/entity/User.java
+++ src/main/java/ro/sd/a2/entity/User.java
@@ -3,8 +3,10 @@ package ro.sd.a2.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "`User`")
 public class User {
 
     @Id
